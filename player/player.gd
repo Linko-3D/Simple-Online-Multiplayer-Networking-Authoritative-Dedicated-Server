ee2afa5e1a63f7bfde8b299b6d4b7ba452f403d9
Linko-3D <dbekhouc@gmail.com>
2025-11-15 09:56:53 +0100

    tweak

--- player/player.gd
+++ player/player.gd
@@ -7,16 +7,16 @@ const SPEED = 500.0
 func _ready():
 	$DisplayControllerPlayer.visible = name.to_int() == multiplayer.get_unique_id()
 	$InputsToServer.set_multiplayer_authority(name.to_int())
-	set_physics_process(multiplayer.is_server())
 
 
 func _physics_process(delta):
-	var direction = $InputsToServer.input_dir
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if multiplayer.is_server():
+		var direction = $InputsToServer.input_dir
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.y = direction.y * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
