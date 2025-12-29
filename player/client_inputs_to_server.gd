0bfd3fa811564e3334c4d07a2578201cf3f74e95
Linko-3D <dbekhouc@gmail.com>
2025-12-29 18:52:26 +0100

    tweak

--- player/client_inputs_to_server.gd
+++ player/client_inputs_to_server.gd
@@ -13,3 +13,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
 		input_dir = Input.get_vector("left", "right", "up", "down")
+
+		# Prevent moving while typing a message
+		if get_tree().get_first_node_in_group("send_message").visible:
+			input_dir = Vector2()
